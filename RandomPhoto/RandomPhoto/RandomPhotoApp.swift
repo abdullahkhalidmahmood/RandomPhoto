6e1d6b7370252187e895be3970e70a23b090b5c7
abdullah.khalid <abdullah.khalid@mekdiz.com>
2021-10-05 10:42:48 +0500

    first push

---
+++ RandomPhoto/RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Abdullah Khalid on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
